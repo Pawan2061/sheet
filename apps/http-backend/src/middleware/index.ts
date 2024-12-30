0cb222327dbacc22de2dad5eaaa8bfa220f28354
Pawan <pandeypawan2061@gmail.com>
2024-12-30 22:03:18 +0530

    feat: basic backend done

--- apps/http-backend/src/middleware/index.ts
+++ apps/http-backend/src/middleware/index.ts
@@ -1,19 +1,34 @@
 import { NextFunction, Request, Response } from "express";
-import jwt, {JwtPayload} from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { JWT_PASSWORD } from "../config";
 
-export function authMiddleware(req: Request, res: Response, next: NextFunction) {
-    const header = req.headers["authorization"];
-    
-    const decoded = jwt.verify(header as string, JWT_PASSWORD);
+export const createToken = async (payload: any) => {
+  const token = jwt.sign(payload, JWT_PASSWORD as string, {
+    expiresIn: "2d",
+  });
+  console.log(token);
 
-    if (decoded) {
-        req.userId = (decoded as JwtPayload)?.userId;
-        next();
-    } else {
-        res.status(403).json({
-            message: "You are not logged in"
-        })
+  return token;
+};
+
+export const authMiddleware = (req: any, res: Response, next: NextFunction) => {
+  const token: any = req.headers.authorization?.split(" ")[1];
+  console.log(token);
+
+  if (!token) {
+    res.status(401).json({ error: "Unauthorized" });
+  }
+  console.log(JWT_PASSWORD, "passwords");
+
+  jwt.verify(token, JWT_PASSWORD as string, (err: any, decodedToken: any) => {
+    console.log(decodedToken, "token was here");
+
+    if (err) {
+      console.log(err);
+
+      return res.json({ error: err.message });
     }
-
-}
+    req.user = decodedToken;
+    next();
+  });
+};
